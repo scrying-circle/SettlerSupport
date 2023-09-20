1c5bb062f8284b9dca9af3a7a70fd7b1de2ba566
Blake O'Callahan <allowed401@gmail.com>
2023-09-20 15:05:49 +1200

    build bugfix

--- app/components/Die.tsx
+++ app/components/Die.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 export default function Die(props: { src: string, color: string, children?: React.ReactNode }) {
     const { src, color, children } = props;
-    function Children(props: { children: React.ReactNode }) {
-        return <>{props.children}</>
-    }
     return <div className="items-center relative" style={{ width: '30vh', height: '30vh' }}>
         <img className='m-0 absolute bottom-[50%] right-[50%] translate-x-1/2 translate-y-1/2 w-full h-full' src={`${color}${src}`} alt='white_face.jpg' />
-        <Children children={children} />
+        {children}
     </div>
 }
